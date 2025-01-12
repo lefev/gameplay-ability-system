e9b3162370342623f061771f062f2bdfbc72c73f
lefev <pascal.lefev@gmail.com>
2025-01-12 10:15:42 +0100

    enables c++20

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -11,6 +11,8 @@ public class Aura : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks" });
+		
+		CppStandard = CppStandardVersion.Cpp20;
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
